a91ae0f3c40d20a62e54399e1b6dbe75cc14f0cc
sairamkiran9 <sairamkiranmuppana.1999@gmail.com>
2023-02-08 14:48:55 -0500

    mymtimes update

--- mymtimes.c
+++ mymtimes.c
@@ -5,94 +5,98 @@
 #include <time.h>
 #include <dirent.h>
 
-void print_mymtimes(int count[]){
-    time_t mytime;
+#define DEFAULT_TIME 86400
+
+void print_mtimes(time_t mytime, int mtimes[])
+{
     struct tm *mytime_info;
     char mytime_str[50];
-    mytime = time(NULL);
     mytime_info = localtime(&mytime);
 
-    // strftime(mytime_str, sizeof(mytime_str), "%a %b %d %H:%M:%S %Y", mytime_info);
-    // printf("%s\n", mytime_str);
-    
+    // Update current time to 24hrs back
     mytime_info->tm_hour -= 24;
     mytime = mktime(mytime_info);
 
+    // Iterate recursively and print number of files modified from the last 24hrs
     for (int i = 0; i < 24; i++)
     {
+        // Using strftime to modify timestamp format as required
         strftime(mytime_str, sizeof(mytime_str), "%a %b %d %H:%M:%S %Y", mytime_info);
-        printf("%s: %d\n", mytime_str, count[mytime_info->tm_hour ]);
-        mytime_info->tm_hour++;
+        printf("%s: %d\n", mytime_str, mtimes[mytime_info->tm_hour]);
+
+        // Increment current time by 1hr for each iteration
+        mytime_info->tm_hour += 1;
         mytime = mktime(mytime_info);
     }
 }
 
 // Function to recursively traverse the directory and its subdirectories
-void check_dir(const char *dir, int *count)
+void get_mtimes(const char *dir, int *mtimes, time_t current_time)
 {
+    char path[1024];
     DIR *directory;
+    time_t file_mtime;
     struct dirent *entry;
     struct stat stat_buf;
-    char path[1024];
-    time_t current_time, file_time;
     struct tm *time_info;
 
     // Check if the directory can be opened
     if (!(directory = opendir(dir)))
         return;
 
-    current_time = time(NULL);
-
     // Read all the entries in the directory
     while ((entry = readdir(directory)) != NULL)
     {
-        // Check if the entry is a regular file
-        if (entry->d_type == DT_REG)
+        snprintf(path, sizeof(path), "%s/%s", dir, entry->d_name);
+        // Check if the entry is a directory
+        if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
         {
-            snprintf(path, sizeof(path), "%s/%s", dir, entry->d_name);
+            // snprintf(path, sizeof(path), "%s/%s", dir, entry->d_name);
+            get_mtimes(path, mtimes, current_time);
+        }
+        // Check if the entry is a regular file
+        else if (entry->d_type == DT_REG)
+        {
+            // snprintf(path, sizeof(path), "%s/%s", dir, entry->d_name);
             if (lstat(path, &stat_buf) == -1)
             {
-                printf("Error: %s\n", "000");
                 closedir(directory);
                 return;
             }
-            file_time = stat_buf.st_mtime;
-
-            // printf("%d\n", file_time);
+            file_mtime = stat_buf.st_mtime;
 
             // Check if the file was modified in the past 24 hours
-            if (difftime(current_time, file_time) <= 86400)
+            if (DEFAULT_TIME >= difftime(current_time, file_mtime))
             {
-                time_info = localtime(&file_time);
-                count[time_info->tm_hour]++;
+                time_info = localtime(&file_mtime);
+                mtimes[time_info->tm_hour] += 1;
             }
         }
-        // Check if the entry is a directory
-        else if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
-        {
-            snprintf(path, sizeof(path), "%s/%s", dir, entry->d_name);
-            check_dir(path, count);
-        }
     }
-
     closedir(directory);
     return;
 }
 
 int main(int argc, char *argv[])
 {
-    int count[24] = {0};
-    char *dir = ".";
-    
-    // Check if a directory path was provided as an argument
+    char *dir = ".";            // default path  
+    int mtimes[24] = {0};
+
+    // Check for directory
     if (argc == 2)
     {
         dir = argv[1];
     }
 
-    check_dir(dir, count);
+    // Fetch current local time
+    time_t current_time;
+    current_time = time(NULL);
 
-    print_mymtimes(count);
+    // Check for files that got modified in the last 24hrs
+    get_mtimes(dir, mtimes, current_time);
+
+    // Print number files that got modified every hr from the last 24hrs
+    print_mtimes(current_time, mtimes);
 
     return 0;
 }
